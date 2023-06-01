add982ae3dbb5c0179d91128dc20cbae74fd44d7
beingtharur <beingtharur@gmail.com>
2023-06-01 18:32:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+	  return (
+		      <div className="app">
+		        <h1>Welcome to Basic React App</h1>
+		        <p>Here are some features and fundamentals of React:</p>
+		        <ul>
+		          <li>Component-based architecture</li>
+		          <li>Virtual DOM for efficient updates</li>
+		          <li>Declarative syntax for building UI</li>
+		          <li>Efficient rendering with React's reconciliation algorithm</li>
+		          <li>State management with hooks and context</li>
+		          <li>Reusable components</li>
+		          <li>React Router for client-side routing</li>
+		        </ul>
+		      </div>
+		    );
+}
+
+export default App;
+
